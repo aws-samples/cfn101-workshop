d473289164c03f21f7dc4c93bda3cb9f6b341f9f
Franco Rezabek <rezabekf@amazon.co.uk>
2022-02-28 14:28:23 +0000

    Fix issues as per feedback

--- code/workspace/package-and-deploy/scripts/deploy-cfn-template.sh
+++ code/workspace/package-and-deploy/scripts/deploy-cfn-template.sh
@@ -2,28 +2,27 @@
 
 # From code/package-and-deploy directory run ./scripts/deploy-cfn-template.sh
 
-BUCKET_NAME="cfn-workshop-s3-s3bucket-1wo3wjmf93viq" # use your own uniquely named S3 bucket
-PREFIX_NAME="lambda-function"
-STACK_NAME="cfn-workshop-lambda"
+BUCKET_NAME="rezabekf-development-bucket" # cfn-workshop-s3bucket-unique-name # use your own uniquely named S3 bucket
+STACK_NAME="cfn-workshop-package-deploy"
 AWS_REGION="eu-west-1" # use the same region as your S3 bucket
 
-printf "\n--> Packaging and uploading templates to the %s S3 bucket ...\n" $BUCKET_NAME
+printf "\n--> Packaging and uploading templates to the %s S3 bucket ...\n" ${BUCKET_NAME}
 
 aws cloudformation package \
-  --template-file infrastructure.template \
-  --s3-bucket $BUCKET_NAME \
-  --s3-prefix $PREFIX_NAME \
-  --output-template-file infrastructure-packaged.template
+  --template-file ./infrastructure.template \
+  --s3-bucket ${BUCKET_NAME} \
+  --s3-prefix ${STACK_NAME} \
+  --output-template-file ./infrastructure-packaged.template
 
 printf "\n--> Validating template ...\n"
 
 aws cloudformation validate-template \
   --template-body file://infrastructure-packaged.template
 
-printf "\n--> Deploying %s template...\n" $STACK_NAME
+printf "\n--> Deploying %s template...\n" ${STACK_NAME}
 
 aws cloudformation deploy \
-  --template-file infrastructure-packaged.template \
-  --stack-name $STACK_NAME \
-  --region $AWS_REGION \
+  --template-file ./infrastructure-packaged.template \
+  --stack-name ${STACK_NAME} \
+  --region ${AWS_REGION} \
   --capabilities CAPABILITY_IAM
--- code/workspace/package-and-deploy/scripts/invoke-lambda-function.sh
+++ code/workspace/package-and-deploy/scripts/invoke-lambda-function.sh
@@ -2,13 +2,18 @@
 
 # From code/package-and-deploy directory run ./scripts/invoke-lambda-function.sh
 
+if [ -z "$1" ]; then
+    echo "Error: Provide time_zone, e.g. Europe/London"
+    exit 1
+else
+  TIMEZONE="$1"
+fi
 LAMBDA_FUNCTION_NAME="cfn-workshop-python-function"
-
-PAYLOAD='{"time_zone":"Europe/London"}'
+PAYLOAD="{\"time_zone\":\"${TIMEZONE}\"}"
 
 aws lambda invoke \
   --function-name $LAMBDA_FUNCTION_NAME \
   --payload $PAYLOAD \
   response.json
 
-echo "$(<response.json)"
+cat response.json
