5bda1d99a2b2a15e4f9fc1979fb83647775d715f
Franco Rezabek <rezabekf@amazon.co.uk>
2022-02-28 14:40:04 +0000

    Also update workspace

--- code/workspace/package-and-deploy/scripts/invoke-lambda-function.sh
+++ code/workspace/package-and-deploy/scripts/invoke-lambda-function.sh
@@ -3,13 +3,13 @@
 # From code/package-and-deploy directory run ./scripts/invoke-lambda-function.sh
 
 if [ -z "$1" ]; then
-    echo "Error: Provide time_zone, e.g. Europe/London"
+    echo "Usage: ./scripts/invoke-lambda-function.sh <TIME_ZONE>, e.g. Europe/London"
     exit 1
 else
-  TIMEZONE="$1"
+  TIME_ZONE="$1"
 fi
 LAMBDA_FUNCTION_NAME="cfn-workshop-python-function"
-PAYLOAD="{\"time_zone\":\"${TIMEZONE}\"}"
+PAYLOAD="{\"time_zone\":\"${TIME_ZONE}\"}"
 
 aws lambda invoke \
   --function-name $LAMBDA_FUNCTION_NAME \
